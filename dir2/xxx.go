cc708df367d99bb312f46ec32bb47aaffbd19994
Brian <brian.leungkp@gmail.com>
2023-10-24 19:07:47 +0800

    updates

---
+++ dir2/xxx.go
@@ -0,0 +1,5 @@
+package dir2
+
+func Test1234() string {
+	return "ok"
+}
--- simple-go-service/main.go
+++ simple-go-service/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/brianlk/simple-go-service/dir1"
+	"github.com/brianlk/simple-go-service/dir2"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(dir1.SayHello())
+	fmt.Println(dir2.Test1234())
 }
